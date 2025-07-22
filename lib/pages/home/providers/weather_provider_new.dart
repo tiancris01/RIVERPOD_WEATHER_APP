65a04e0d84016c5faefdea9403484e81f181fa23
Tiancris <103019305+tiancris01@users.noreply.github.com>
2025-07-22 18:44:25 -0500

    [ft][CD-006] Refactor weather provider implementation with improved UX and code generation (#8)

--- lib/pages/home/providers/weather_provider_new.dart
+++ lib/pages/home/providers/weather_provider_new.dart
@@ -4,7 +4,7 @@ import 'package:weather_riverpod_app/models/mappers/weather_mapper.dart';
 import 'package:weather_riverpod_app/repositories/providers/weather_repository.dart';
 import 'package:weather_riverpod_app/entities/weather/weather_entity.dart';
 
-part 'weather_provider.g.dart';
+part 'weather_provider_new.g.dart';
 
 @riverpod
 class Weather extends _$Weather {
@@ -62,35 +62,3 @@ class LoadingState extends _$LoadingState {
     state = loading;
   }
 }
-
-// @Riverpod(keepAlive: true)
-// Future<List<WeatherEntity?>> weatherEntity(WeatherEntityRef ref) async {
-//   ref.onDispose(() {
-//     print('WeatherEntityProvider disposed');
-//   });
-//   final weatherState = await ref.watch(weatherProvider.future);
-//   return [weatherState];
-// }
-
-// @riverpod
-// FutureOr<void> weather(WeatherRef ref, {required String city}) async {
-//   final weatherRepository = ref.watch(weatherRepositoryProvider);
-//   final CurrentWeather currentWeather = await weatherRepository.fetchWeather(
-//     city: city,
-//   );
-//   print('currentWeather: $currentWeather');
-//   final WeatherEntity state = WeatherMapper.weatherMapper(currentWeather);
-//   ref.read(weatherEntityProviderProvider.notifier).addWeather(state);
-// }
-
-// @Riverpod(keepAlive: true)
-// class WeatherEntityProvider extends _$WeatherEntityProvider {
-//   @override
-//   List<WeatherEntity> build() {
-//     return [];
-//   }
-
-//   void addWeather(WeatherEntity weather) {
-//     state = [...state, weather];
-//   }
-// }
